d7b01b08fe5db47ec11649f2ba2b3a608d38fb37
Anton Dachin <relas1234@gmail.com>
2022-01-09 14:39:45 +0300

    Basic type typescript

--- test.ts
+++ test.ts
@@ -1,5 +1,41 @@
-const a: number = 5;
+let a: number = 4;
+let b: string = "test";
+let c: boolean = true;
 
-// ГАВ
-//! Гав2
-//? Гав3
+let d: string[] = ['1', '2'];
+
+let e: any = 3;
+
+function test(a: string): string | number {
+	return '';
+}
+
+
+const test2 = (a: number): number => {
+	return a * 2;
+}
+
+d = d.map(x => x.toLowerCase());
+
+function countCoord(coord: { lat: number, long?: number }) {
+
+}
+
+
+function print(id: number | string) {
+	if (typeof id === 'number') {
+		console.log(id.toString());
+	} else if (typeof id === "string") {
+		id.toUpperCase();
+	}
+}
+
+function getSum(a: number | number[]) {
+	if (Array.isArray(a)) {
+		a.sort();
+	}
+}
+
+function aaa(): void {
+
+}
