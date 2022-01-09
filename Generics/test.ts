75c5867882ac408c40009d24c18fd51ef156609f
Anton Dachin <relas1234@gmail.com>
2022-01-09 20:48:16 +0300

    Add Generics

---
+++ Generics/test.ts
@@ -0,0 +1,42 @@
+function logTime(num: number): number {
+	console.log(new Data());
+	return num;
+}
+
+function logTime1(str: string): string {
+	console.log(new Data());
+	return str;
+}
+//! Выше дублирование. It's bad. Решим это... (any не выход)
+
+
+function logTimeGeneric<T>(num: T): T {
+	if (typeof num === 'string') {
+		num.toUpperCase();
+	}
+	console.log(new Data());
+	return num;
+}
+
+
+logTimeGeneric<string>('asd');
+logTimeGeneric<number>(5);
+
+interface MyInterface {
+	transform: <T, F>(a: T) => F
+}
+
+class MyGenClass<T> {
+	value!: T
+}
+
+const hhh = new MyGenClass<number>();
+
+interface TimeStamp {
+	stamp: number;
+}
+
+function logTimeStamp<T>(num: T): T {
+	console.log(num.stamp);
+	return num;
+}
